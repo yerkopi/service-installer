8edf7ced1879f51e5792b836049d10cdce01162b
machinetherapist <79029454+illegal-instruction-co@users.noreply.github.com>
2023-08-17 11:36:09 +0300

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERNAME="yerkopi"
+TARGET_FOLDER="/home/yerkopi"
+ORGANIZATION="yerkopi"
+INSTALL_SCRIPT="install.sh"
+
+REPO_LIST=$(curl -s "https://api.github.com/orgs/$ORGANIZATION/repos" | grep '"name":' | awk -F'"' '{print $4}')
+
+for REPO in $REPO_LIST; do
+    if [[ $REPO == *"-service"* ]]; then
+        git clone "https://github.com/$ORGANIZATION/$REPO" "$TARGET_FOLDER/$REPO"
+        
+        if [ -f "$TARGET_FOLDER/$REPO/$INSTALL_SCRIPT" ]; then
+            chmod +x "$TARGET_FOLDER/$REPO/$INSTALL_SCRIPT"
+            "$TARGET_FOLDER/$REPO/$INSTALL_SCRIPT"
+        else
+            echo "Install script ($INSTALL_SCRIPT) not found for $REPO."
+        fi
+    fi
+done
+
+echo "Script completed."
