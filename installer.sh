e8027388d13a7ffbbfa87dcf413277a134a34e1a
machinetherapist <79029454+illegal-instruction-co@users.noreply.github.com>
2023-08-17 17:42:49 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,6 +4,7 @@ USERNAME="yerkopi"
 TARGET_FOLDER="/home/yerkopi"
 ORGANIZATION="yerkopi"
 INSTALL_SCRIPT="script/install.sh"
+UPDATE_SCRIPT="script/install.sh"
 
 REPO_LIST=$(curl -s "https://api.github.com/orgs/$ORGANIZATION/repos" | grep '"name":' | awk -F'"' '{print $4}')
 
@@ -16,6 +17,12 @@ for REPO in $REPO_LIST; do
             "$TARGET_FOLDER/$REPO/$INSTALL_SCRIPT"
         else
             echo "Install script ($INSTALL_SCRIPT) not found for $REPO."
+        fi
+        if [ -f "$TARGET_FOLDER/$REPO/$UPDATE_SCRIPT" ]; then
+            sudo chmod +x "$TARGET_FOLDER/$REPO/$UPDATE_SCRIPT"
+            "$TARGET_FOLDER/$REPO/$UPDATE_SCRIPT"
+        else
+            echo "Update script ($UPDATE_SCRIPT) not found for $REPO."
         fi
     fi
 done
